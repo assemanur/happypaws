3863c6ea57ddfc23ad52975753cf55ab441bd00f
assemanur <asemyara.n@gmail.com>
2023-01-17 13:26:43 -0800

    Added mailto feature for user to be able to email organizations and send inquires re: animal. Organization can be viewed on the google map.

--- static/js/map.js
+++ static/js/map.js
@@ -43,3 +43,43 @@ function initMap() {
   animalInfo.open(basicMap, animalMarker);
 });
 }
+
+function initOrgMap() {
+
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&key=AIzaSyAu7-wLA-0gHG1wQs2uRhuGtaDpyV6hBp0`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    
+    console.log(data);
+    console.log(data.results[0].geometry.location);
+    data.results[0].geometry.location;
+    const organizationCoords = {
+        lat: data.results[0].geometry.location.lat,
+        lng: data.results[0].geometry.location.lng,
+      };
+
+  const basicMap = new google.maps.Map(document.querySelector('#map'),{
+    center: organizationCoords,
+    zoom: 12,
+  });
+  
+
+  const organizationMarker = new google.maps.Marker({
+    position: organizationCoords,
+    title: 'organization',
+    map: basicMap,
+  });
+
+  organizationMarker.addListener('click', () => {
+    alert('Hi!');
+  });
+
+  const organizationInfo = new google.maps.InfoWindow({
+    content: '<p>We have the cutest animals.</p>',
+  });
+
+  organizationInfo.open(basicMap, organizationMarker);
+});
+}
